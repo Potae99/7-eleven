500d8a4517912a993118a00ea12e54c7f06964e2
Potae99 <101413178+Potae99@users.noreply.github.com>
2024-01-03 08:43:01 +0700

    Passed 1st first test

---
+++ tests/seven_eleven/test_7-eleven.py
@@ -0,0 +1,15 @@
+"""Test case for 7-11 package."""
+
+#St lib
+
+
+#3rd party lib
+import pytest
+
+
+#Project lib
+# from seven_eleven.seven from print_7_eleven
+
+#----------------------------------------------------------------------------------------------------------------
+def test_print_7_eleven():
+    """Print 7-eleven"""
